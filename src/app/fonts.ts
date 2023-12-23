3e3c1a1b473fc553d352f22e2fb9cd2555b6d52e
Mikoyzskie <rapidstrike13@gmail.com>
2023-12-23 09:29:02 +0800

    draft done

---
+++ src/app/fonts.ts
@@ -0,0 +1,3 @@
+import { Inter, Lexend_Deca } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
+export const lexend = Lexend_Deca({ weight: "400", subsets: ["latin"] });
